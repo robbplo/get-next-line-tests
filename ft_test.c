24e40bcdeaed621bc49650862d8545d93518a6a5
Robbin Ploeger <robbplo@gmail.com>
2025-10-22 12:43:41 +0200

    Add better print statements

--- ft_test.c
+++ ft_test.c
@@ -6,34 +6,33 @@
 /*   By: rploeger <rploeger.student@codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 19:38:30 by rploeger          #+#    #+#             */
-/*   Updated: 2025/10/20 17:54:25 by rploeger         ###   ########.fr       */
+/*   Updated: 2025/10/22 12:41:20 by rploeger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// todo: multiple consecutive newlines print remaining data in buffer
-
 void	test1()
 {
 	char *str;
 	int fd;
 
-	printf("\nTest 1\n");
-	fd = open("./files/test1", O_RDONLY);
-	for (int i = 0; i < 11; i++) {
+	printf("\nTest pyramid of 9s\n");
+	fd = open("./files/pyramid", O_RDONLY);
+	for (int i = 0; i < 10; i++) {
 		str = get_next_line(fd);
 		printf("%s", str);
-		if (str)
-			assert(strlen(str) == i + 2);
+		assert(strlen(str) == i + 2);
 		free(str);
 	}
+	assert(get_next_line(fd) == NULL);
 }
 
 void	test2()
@@ -41,8 +40,8 @@ void	test2()
 	char *str;
 	int fd;
 
-	printf("\nTest 2\n");
-	fd = open("./files/test2", O_RDONLY);
+	printf("\nTest with one line followed by two empty lines\n");
+	fd = open("./files/one-line-two-empty", O_RDONLY);
 	str = get_next_line(fd);
 	printf("%s", str);
 	assert(!strcmp("123456789\n", str));
@@ -55,9 +54,7 @@ void	test2()
 	printf("%s", str);
 	assert(!strcmp("\n", str));
 	free(str);
-	str = get_next_line(fd);
-	printf("%s", str);
-	assert(str == NULL);
+	assert(get_next_line(fd) == NULL);
 }
 
 void	test3()
@@ -65,7 +62,7 @@ void	test3()
 	char *str;
 	int fd;
 
-	printf("\nTest 3\n");
+	printf("\nTest one line with two thousand characters\n");
 	fd = open("./files/two-thousand", O_RDONLY);
 	str = get_next_line(fd);
 	printf("%s", str);
@@ -73,6 +70,7 @@ void	test3()
 	assert(str[1000] == '1');
 	assert(str[1999] == '1');
 	assert(str[2000] == '\n');
+	assert(get_next_line(fd) == NULL);
 	free(str);
 }
 
@@ -81,7 +79,7 @@ void	test4()
 	char *str;
 	int fd;
 
-	printf("\nTest 4\n");
+	printf("\nTest one line with a million characters\n");
 	fd = open("./files/million", O_RDONLY);
 	str = get_next_line(fd);
 	printf("one million test\n");
@@ -90,7 +88,16 @@ void	test4()
 	assert(str[10000] == '1');
 	assert(str[100000] == '1');
 	assert(str[1000000] == '\n');
+	assert(get_next_line(fd) == NULL);
 	free(str);
+}
+
+void	test5()
+{
+	printf("\nTest with invalid file descriptors\n");
+	assert(get_next_line(-1) == NULL);
+	assert(get_next_line(INT32_MAX) == NULL);
+	assert(get_next_line(INT32_MIN) == NULL);
 }
 
 int main(int argc, char **argv)
@@ -99,10 +106,11 @@ int main(int argc, char **argv)
 	int fd;
 
 	if (argc == 1) {
-		// test1();
-		// test2();
-		// test3();
+		test1();
+		test2();
+		test3();
 		test4();
+		test5();
 	} 
 	if (argc == 2) {
 		if (!strcmp("-", argv[1]))
