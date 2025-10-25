148a627c582e9130910af2cbd929f9e92cc03e98
Robbin Ploeger <robbplo@gmail.com>
2025-10-25 11:26:45 +0200

    Add tests for different buffer sizes

--- Makefile
+++ Makefile
@@ -1,18 +1,41 @@
-CC = cc
-CFLAGS = -g -I$(LIBNEXTLINE) -fsanitize=address
-LDFLAGS = -L$(LIBNEXTLINE) -lnextline
-SRCS = 
+CC = clang
+CFLAGS = -g -I.. -fsanitize=address
+LDFLAGS = -L..
 OUT = test
 LIBNEXTLINE = ..
 
-main: libnextline
-	$(CC) $(CFLAGS) $(SRCS) ft_test.c $(LDFLAGS) -o $(OUT)
 
-libnextline:
-	make -C $(LIBNEXTLINE)
+test: test_1 test_42 test_10m
 
-fclean:
-	rm -f $(OUT)
+m_1:
+	@$(CC) -D="BUFFER_SIZE 1" -fsanitize=address ../get_next_line.c ../get_next_line_utils.c -c
+	@$(CC) $(CFLAGS) ../get_next_line.o ../get_next_line_utils.o ft_test.c $(LDFLAGS) -o $(OUT)
 
-test: main
-	./$(OUT)
+m_42:
+	@$(CC) -D="BUFFER_SIZE 42" -fsanitize=address ../get_next_line.c ../get_next_line_utils.c -c
+	@$(CC) $(CFLAGS) ../get_next_line.o ../get_next_line_utils.o ft_test.c $(LDFLAGS) -o $(OUT)
+
+m_10m:
+	@$(CC) -D="BUFFER_SIZE 1000000" -fsanitize=address ../get_next_line.c ../get_next_line_utils.c -c
+	@$(CC) $(CFLAGS) ../get_next_line.o ../get_next_line_utils.o ft_test.c $(LDFLAGS) -o $(OUT)
+
+test_1: m_1
+	@echo "================================="
+	@echo "= Testing BUFFER_SIZE 1         ="
+	@echo "================================="
+	@./$(OUT)
+	@echo
+
+test_42: m_42
+	@echo "================================="
+	@echo "= Testing BUFFER_SIZE 42        ="
+	@echo "================================="
+	@./$(OUT)
+	@echo
+
+test_10m: m_10m
+	@echo "================================="
+	@echo "= Testing BUFFER_SIZE 10000000  ="
+	@echo "================================="
+	@./$(OUT)
+	@echo
--- ft_test.c
+++ ft_test.c
@@ -24,11 +24,10 @@ void	test1()
 	char *str;
 	int fd;
 
-	printf("\nTest pyramid of 9s\n");
+	printf("Test pyramid of 9s\n");
 	fd = open("./files/pyramid", O_RDONLY);
 	for (int i = 0; i < 10; i++) {
 		str = get_next_line(fd);
-		printf("%s", str);
 		assert(strlen(str) == i + 2);
 		free(str);
 	}
@@ -41,18 +40,15 @@ void	test2()
 	char *str;
 	int fd;
 
-	printf("\nTest with one line followed by two empty lines\n");
+	printf("Test with one line followed by two empty lines\n");
 	fd = open("./files/one-line-two-empty", O_RDONLY);
 	str = get_next_line(fd);
-	printf("%s", str);
 	assert(!strcmp("123456789\n", str));
 	free(str);
 	str = get_next_line(fd);
-	printf("%s", str);
 	assert(!strcmp("\n", str));
 	free(str);
 	str = get_next_line(fd);
-	printf("%s", str);
 	assert(!strcmp("\n", str));
 	free(str);
 	assert(get_next_line(fd) == NULL);
@@ -63,10 +59,9 @@ void	test3()
 	char *str;
 	int fd;
 
-	printf("\nTest one line with two thousand characters\n");
+	printf("Test one line with two thousand characters\n");
 	fd = open("./files/two-thousand", O_RDONLY);
 	str = get_next_line(fd);
-	printf("%s", str);
 	assert(str[0] == '1');
 	assert(str[1000] == '1');
 	assert(str[1999] == '1');
@@ -80,7 +75,7 @@ void	test4()
 	char *str;
 	int fd;
 
-	printf("\nTest one line with a million characters\n");
+	printf("Test one line with a million characters\n");
 	fd = open("./files/million", O_RDONLY);
 	str = get_next_line(fd);
 	assert(str[0] == '1');
@@ -94,7 +89,7 @@ void	test4()
 
 void	test5()
 {
-	printf("\nTest with invalid file descriptors\n");
+	printf("Test with invalid file descriptors\n");
 	assert(get_next_line(-1) == NULL);
 	assert(get_next_line(INT32_MAX) == NULL);
 	assert(get_next_line(INT32_MIN) == NULL);
@@ -105,18 +100,16 @@ void test6()
 	char *str;
 	int fd;
 
-	printf("\nTest one char\n");
+	printf("Test one char\n");
 	fd = open("./files/1char", O_RDONLY);
 	str = get_next_line(fd);
-	printf("%s", str);
 	assert(str[0] == '1');
 	assert(get_next_line(fd) == NULL);
 	free(str);
 
-	printf("\nTest one char without newline\n");
+	printf("Test one char without newline\n");
 	fd = open("./files/1char-no-nl", O_RDONLY);
 	str = get_next_line(fd);
-	printf("%s", str);
 	assert(str[0] == '1');
 	assert(get_next_line(fd) == NULL);
 	free(str);
@@ -127,7 +120,7 @@ void test7()
 	char *str;
 	int fd;
 
-	printf("\nTest alternating line with newline\n");
+	printf("Test alternating line with newline\n");
 	fd = open("./files/alternate_lines", O_RDONLY);
 	str = get_next_line(fd);
 	assert(!strcmp(str, "98765432109876543210987654321098765432109\n"));
